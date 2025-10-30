20bc03f2b3864942a434321f087839528af97df8
shiranAharoni <shiran5725@gmail.com>
2025-10-30 13:42:41 +0200

    Add 12.password-checker project

---
+++ 12.password-checker/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/" , (req , res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check" ,(req, res) =>{
+    console.log(req.body);
+    if(req.body["password"] === "ProgrammingLover"){
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
+});
+
+
+app.listen(port, () =>{
+    console.log(`listening on port: ${port}.`);
+});
